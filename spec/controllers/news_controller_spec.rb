8287db8809b0f8fc638c319f81afbaf5fc3ce0ad
oiizes <oiizesgitlab@gmail.com>
2019-10-24 16:29:33 +0300

    rspec test

--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -1,3 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 
+RSpec.describe NewsController, type: :controller do
+  describe '#world_news' do
+    it 'should return success response' do 
+        get :world_news
+        expect(response).to have_http_status(:ok)
+    end
+  end
+end
